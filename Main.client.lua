4fe98c2ac00486322b728aa75c3d2d09bf79ae1a
codeccv4-arch <codeccv4@gmail.com>
2026-05-13 23:03:41 +0700

    Update Main.client.lua

--- Main.client.lua
+++ Main.client.lua
@@ -1,1 +1,17 @@
+--// Main.client.lua
+local Manager = require(script:WaitForChild("Manager"))
 
+local Modules = Manager.LoadAll()
+
+-- สร้าง UI
+local gui, frame, toggle, btn = Modules.UI.Create()
+
+-- เปิดระบบทั้งหมด
+Modules.Effects.Start(gui, frame, toggle, btn)
+Modules.Drag.Enable(frame)
+Modules.Button.Bind(btn)
+
+-- toggle menu
+toggle.MouseButton1Click:Connect(function()
+	Modules.UI.Toggle(frame)
+end)
