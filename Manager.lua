a08d635418dc601e8acb6b10e88bee144a46160a
codeccv4-arch <codeccv4@gmail.com>
2026-05-13 23:03:07 +0700

    Update Manager.lua

--- Manager.lua
+++ Manager.lua
@@ -1,1 +1,23 @@
+--// Manager.lua
+local Manager = {}
 
+function Manager.LoadAll()
+	local modulesFolder = script:WaitForChild("Modules")
+	local loaded = {}
+
+	for _, module in ipairs(modulesFolder:GetChildren()) do
+		if module:IsA("ModuleScript") then
+			local ok, result = pcall(require, module)
+
+			if ok then
+				loaded[module.Name] = result
+			else
+				warn("[Module Error]", module.Name, result)
+			end
+		end
+	end
+
+	return loaded
+end
+
+return Manager
