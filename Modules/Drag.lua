de30a9000fc91e3e9e30f212e4074552547488ce
codeccv4-arch <codeccv4@gmail.com>
2026-05-13 23:05:25 +0700

    Update Drag.lua

--- Modules/Drag.lua
+++ Modules/Drag.lua
@@ -1,1 +1,39 @@
+--// Modules/Drag.lua
+local Drag = {}
 
+function Drag.Enable(frame)
+	local UIS = game:GetService("UserInputService")
+
+	local dragging = false
+	local dragStart
+	local startPos
+
+	frame.InputBegan:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseButton1 then
+			dragging = true
+			dragStart = input.Position
+			startPos = frame.Position
+		end
+	end)
+
+	frame.InputEnded:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseButton1 then
+			dragging = false
+		end
+	end)
+
+	UIS.InputChanged:Connect(function(input)
+		if dragging and input.UserInputType == Enum.UserInputType.MouseMovement then
+			local delta = input.Position - dragStart
+
+			frame.Position = UDim2.new(
+				startPos.X.Scale,
+				startPos.X.Offset + delta.X,
+				startPos.Y.Scale,
+				startPos.Y.Offset + delta.Y
+			)
+		end
+	end)
+end
+
+return Drag
