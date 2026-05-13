68374f947ace751acbcf8a87621beb0350f429a7
codeccv4-arch <codeccv4@gmail.com>
2026-05-13 23:06:03 +0700

    Update Button.lua

--- Modules/Button.lua
+++ Modules/Button.lua
@@ -1,1 +1,12 @@
+--// Modules/Button.lua
+local Button = {}
 
+function Button.Bind(btn)
+	btn.MouseButton1Click:Connect(function()
+		btn.Text = "CLICKED!"
+		task.wait(0.3)
+		btn.Text = "TEST BUTTON"
+	end)
+end
+
+return Button
